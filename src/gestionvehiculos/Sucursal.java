44351fc5b0e17fb0241315b2861a8e20d764c671
Plaza Vespucio <pvespucio@PVAWSB03LC0314.duoc.acad>
2024-10-21 19:45:59 -0300

    Update Sucursal.java
    everything fine

--- src/gestionvehiculos/Sucursal.java
+++ src/gestionvehiculos/Sucursal.java
@@ -1,5 +1,43 @@
 package gestionvehiculos;
-
+import java.util.ArrayList;
 public class Sucursal {
+    private String idSucursal;
+    private String nombre;
+    private ArrayList<Vehiculo> vehiculo = new ArrayList();
 
+    public Sucursal() {
+    }
+
+    public Sucursal(String idSucursal, String nombre) {
+        this.idSucursal = idSucursal;
+        this.nombre = nombre;
+    }
+
+    public String getIdSucursal() {
+        return idSucursal;
+    }
+
+    public void setIdSucursal(String idSucursal) {
+        this.idSucursal = idSucursal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Vehiculo> getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(ArrayList<Vehiculo> vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+    
+    public void agregarVehiculo (Vehiculo vehiculo){
+        this.vehiculo.add(vehiculo);
+    }
 }
