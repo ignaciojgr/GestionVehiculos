24d642edac4263868d51aa040e82198b8b434f59
ignaciojgr <ignac.gonzalezr@duocuc.cl>
2024-11-13 00:20:11 -0300

    ok

--- SistemaGestionInventarioConGUI/src/sistemagestioninventariocongui/Proyecto.java
+++ SistemaGestionInventarioConGUI/src/sistemagestioninventariocongui/Proyecto.java
@@ -3,6 +3,7 @@ package sistemagestioninventariocongui;
 import java.util.ArrayList;
 
 public class Proyecto {
+
     private Integer idProyecto;
     private String nombre;
     private ArrayList<Task> tasks = new ArrayList();
@@ -38,6 +39,58 @@ public class Proyecto {
     public void setTasks(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
+
+    public void addTask(Task task) {
+        for (Task iterador : tasks) {
+            if (iterador.getIdTask().equals(task.getIdTask())) {
+                System.out.println("Tarea ya existente. ");
+                return;
+            }
+        }
+        this.tasks.add(task);
+    }
+
+    public void updateTask(Task updatedTask) {
+        for (Task task : tasks) {
+            if (task.getIdTask().equals(updatedTask.getIdTask())) {
+                task.setNombre(updatedTask.getNombre());
+                task.setFechaLimite(updatedTask.getFechaLimite());
+                task.setPrioridad(updatedTask.getPrioridad());
+                task.setCompletada(updatedTask.getCompletada());
+                System.out.println("Task updated. New attributes values: \n"
+                        + "ID: " + task.getIdTask() + "\n"
+                        + "Name: " + task.getNombre() + "\n"
+                        + "Fecha limite: " + task.getFechaLimite() + "\n"
+                        + "Prioridad: " + task.getPrioridad() + "\n"
+                        + "Completada: " + task.getCompletada());
+                return;
+            }
+        }
+    }
     
+    public void readTask(){
+        Integer contador = 0;
+        for (Task task : tasks){
+            contador += 1;
+            System.out.println("Reading tasks ... \n"
+                            + "TASK " + contador + "\n"
+                            + "------------------------\n"
+                            + "ID: " + task.getIdTask() + "\n"
+                            + "Name: " + task.getNombre() + "\n"
+                            + "Fecha limite: " + task.getFechaLimite() + "\n"
+                            + "Prioridad: " + task.getPrioridad() + "\n"
+                            + "Completada: " + task.getCompletada());
+            return;
+        }
+    }
     
+    public void removeTask(Task task){
+        for (Task t : tasks) {
+            if (t.getIdTask().equals(task.getIdTask())) {
+                tasks.remove(t);
+                System.out.println("Task removed.");
+                return;
+            }
+        }
+    }
 }
--- SistemaGestionInventarioConGUI/src/sistemagestioninventariocongui/SistemaGestionInventarioConGUI.java
+++ SistemaGestionInventarioConGUI/src/sistemagestioninventariocongui/SistemaGestionInventarioConGUI.java
@@ -1,20 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package sistemagestioninventariocongui;
 
-/**
- *
- * @author ignac
- */
+import java.time.LocalDateTime;
+
+
 public class SistemaGestionInventarioConGUI {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        LocalDateTime fechaLimite = LocalDateTime.of(2023, 11, 13, 0, 0);
+        Task t1 = new Task(1, "nn", fechaLimite, 1, false);
+        Task t2 = new Task(2, "oo", fechaLimite, 3, false);
+        Task updateTask1 = new Task(1, "Hola", fechaLimite, 2, true);
+        Proyecto p1 = new Proyecto();
+        p1.addTask(t1);
+        p1.addTask(t2);
+        p1.readTask();
+        p1.updateTask(updateTask1);
+        p1.readTask();
+        p1.removeTask(t1);
+        p1.readTask();
     }
     
 }
